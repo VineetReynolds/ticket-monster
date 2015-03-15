7a366de4a1b6f88af7ecbc68073c2a3a71bd983a
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2015-03-16 03:05:45 +0530

    Added a representation class for use in REST API

---
+++ demo/src/main/java/org/jboss/jdf/example/ticketmonster/rest/Carrier.java
@@ -0,0 +1,35 @@
+package org.jboss.jdf.example.ticketmonster.rest;
+
+/**
+ * This is a representation bean class to represent a shipment carrier
+ */
+public class Carrier {
+
+    private long id;
+    private String name;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
